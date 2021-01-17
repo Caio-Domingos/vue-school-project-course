423ac538d2c7d665982326e438858d5adad3ead0
Caio Domingos <caiod.dev@gmail.com>
2021-01-17 17:47:08 -0300

    1 version only students

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Alunos />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Alunos from './components/Aluno/Alunos.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Alunos,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #eee;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  justify-items: center;
+}
+body,
+html {
+  margin: 0;
+  height: 100%;
+}
+
+table {
+  margin: 0px;
+  padding: 0px;
+  list-style-type: none;
+  width: 100%;
+}
+table tr td {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+table thead th {
+  background-color: rgb(184, 208, 216) !important;
+  font-size: 1.2em;
+  padding: 10px 0px;
+  text-align: center !important;
+}
+.colPequeno {
+  width: 5%;
+}
+
+.btn {
+  background-color: #fa4430;
+  padding: 10px 20px;
+  border: 1px solid #000;
+  cursor: pointer;
+  font-weight: bold;
+  color: white;
+  border-radius: 5px;
+  border-bottom-width: 3px;
+}
+
+.btn:hover {
+  box-shadow: 1px 1px 1px #000;
 }
 </style>
---
+++ src/components/Aluno/Alunos.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <Title text="Alunos" />
+    <div class="input-content">
+      <input
+        type="text"
+        name="aluno"
+        id="aluno"
+        placeholder="Nome do Aluno"
+        v-model="name"
+        v-on:keyup.enter="addStudent()"
+      />
+      <button class="btn btn-input" @click="addStudent()">+</button>
+    </div>
+    <br />
+    <table>
+      <thead>
+        <th>Mat.</th>
+        <th>Nome</th>
+        <th>Opcoes</th>
+      </thead>
+      <tbody v-if="students.length > 0">
+        <tr v-for="(student, index) in students" :key="index">
+          <td>{{ index + 1 }}</td>
+          <td>{{ student.name }}</td>
+          <td>
+            <button class="btn" @click="remove(index)">Remover</button>
+          </td>
+        </tr>
+      </tbody>
+      <tfoot v-else>
+        Nenhum aluno encontrado
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import Title from './../_Share/Title';
+export default {
+  components: { Title },
+  data() {
+    return {
+      title: 'Aluno',
+      name: '',
+      students: [
+        {
+          id: 1,
+          name: 'Marcos',
+        },
+        {
+          id: 1,
+          name: 'Carlos',
+        },
+        {
+          id: 1,
+          name: 'Isabela',
+        },
+      ],
+    };
+  },
+  props: {},
+  methods: {
+    addStudent() {
+      this.students.push({ id: this.students.length, name: this.name });
+      this.name = '';
+      console.log('my students', this.students);
+    },
+    remove(index) {
+      this.students.splice(index, 1);
+      console.log('my students', this.students);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-content {
+  display: flex;
+}
+
+input {
+  flex: 1;
+  width: 100%;
+  margin-right: 10px;
+  border: 0;
+  padding: 20px;
+  font-size: 1.3rem;
+  color: #687f7f;
+}
+
+.btn-input {
+  width: 150px;
+  border: 0px;
+  padding: 20px;
+  font-size: 1.3rem;
+  background-color: rgb(115, 115, 115);
+}
+</style>
